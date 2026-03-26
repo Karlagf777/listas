4e787f6958e1b9bb9302d652269b4072cd8b9fe9
KarlaMinely7 <karla.gf.upaep@gmail.com>
2026-03-26 08:37:56 -0600

    Create 2codigo.cs

---
+++ 2codigo.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Inventario 
+{
+    List<Producto> productos = new List<Producto>();
+    int contadorId
+    {
+        //Lista
+    AgregarProducto(nombre, stock)
+    ListaProductos()
+    BuscarProducto(nombre)
+    ActualizarStock(id, nuevoStock)
+    EliminarProducto(id)
+    }
+}
+public class Producto 
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public int stock { get; set; }
+}
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
